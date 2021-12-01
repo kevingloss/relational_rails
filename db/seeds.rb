03494f495f25f060224e1f25799af4ae71ffe2f6
Kevin Gloss <kgloss89@gmail.com>
2021-12-01 16:02:29 -0700

    Add seed variables to populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@author = Author.create!(name: 'Robert Jordan', alive: false, age: 58)
+@author_2 = Author.create!(name: 'Brandon Sanderson', alive: true, age: 45)
+@author_3 = Author.create!(name: 'Brent Weeks', alive: true, age: 44)
+
+@book = @author.books.create!(name: 'The Eye of The World', rating: 4.5, read: true)
+@book_2 = @author.books.create!(name: 'The Great Hunt', rating: 4.3, read: true)
+@book_3 = @author_2.books.create!(name: 'The Way of Kings', rating: 4.2, read: true)
+@book_4 = @author_3.books.create!(name: 'Black Prism', rating: 3.5, read: true)
+@book_5 = @author_3.books.create!(name: 'Night Angel', rating: 3.8, read: true)
