736f3b5571560cce21f9e4c6a960f71da304ad6c
Kevin Gloss <kgloss89@gmail.com>
2021-12-02 10:05:44 -0700

    Add testing to model/show for total_books

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -2,18 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'authors show page', type: :feature do
   describe 'when I visit author/:author_id' do
+
+    before :each do
+      @author = Author.create!(name: 'Robert Jordan', alive: false, age: 58)
+      @author_2 = Author.create!(name: 'Brandon Sanderson', alive: true, age: 45)
+      @author_3 = Author.create!(name: 'Brent Weeks', alive: true, age: 44)
+
+      @book = @author.books.create!(name: 'The Eye of The World', rating: 4.5, read: true)
+      @book_2 = @author.books.create!(name: 'The Great Hunt', rating: 4.3, read: true)
+      @book_3 = @author_2.books.create!(name: 'The Way of Kings', rating: 4.2, read: true)
+      @book_4 = @author_3.books.create!(name: 'Black Prism', rating: 3.5, read: true)
+      @book_5 = @author_3.books.create!(name: 'Night Angel', rating: 3.8, read: true)
+    end
+
     it 'displays the author and attributes' do
-      author = Author.create!(name: 'Robert Jordan', alive: false, age: 58)
-      author_2 = Author.create!(name: 'Brandon Sanderson', alive: true, age: 45)
-      author_3 = Author.create!(name: 'Brent Weeks', alive: true, age: 44)
+      visit "authors/#{@author.id}"
 
-      visit "authors/#{author.id}"
+      expect(page).to have_content(@author.name)
+      expect(page).to have_content(@author.alive)
+      expect(page).to have_content(@author.age)
+      expect(page).to have_content(@author.created_at)
+      expect(page).to have_content(@author.updated_at)
+    end
 
-      expect(page).to have_content(author.name)
-      expect(page).to have_content(author.alive)
-      expect(page).to have_content(author.age)
-      expect(page).to have_content(author.created_at)
-      expect(page).to have_content(author.updated_at)
+    it 'displays the count of books associated with the author' do
+      visit "authors/#{@author_3.id}"
+
+      expect(page).to have_content("Total Books: 2")
     end
   end
 end
