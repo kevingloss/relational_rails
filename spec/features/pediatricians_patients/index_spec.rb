3e60963267e737e89fe3475b5eded6937797597f
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-02 19:26:28 -0700

    Add links to Pediatrician and Patient Index to pediatricians_patients

--- spec/features/pediatricians_patients/index_spec.rb
+++ spec/features/pediatricians_patients/index_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Pediatricians Patients Index' do
-  it 'displays a Pediatricians patients' do
-    pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
-    pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
-    baby = Patient.create!(name: 'Elora Mielke', age_in_months: 2, full_term_birth: true, pediatrician_id: pediatrician.id)
-
-    visit "/pediatricians/#{pediatrician.id}/patients"
-
-    expect(page).to have_content(baby.name)
-    expect(page).to have_content(baby.age_in_months)
-    expect(page).to have_content(baby.full_term_birth)
+  before :each do
+    @pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
+    @pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+    @baby = Patient.create!(name: 'Elora Mielke', age_in_months: 2, full_term_birth: true, pediatrician_id: @pediatrician.id) 
   end
 
-  it 'displays a link to '
+  it 'displays a Pediatricians patients' do
+
+    visit "/pediatricians/#{@pediatrician.id}/patients"
+
+    expect(page).to have_content(@baby.name)
+    expect(page).to have_content(@baby.age_in_months)
+    expect(page).to have_content(@baby.full_term_birth)
+  end
+
+  it 'displays a link to take the user to the Pediatricians Index' do
+
+    visit "/pediatricians/#{@pediatrician.id}/patients"
+
+    click_on "Pediatricians"
+
+    expect(current_path).to eq('/pediatricians')
+  end
+
+  it 'displays a link to take the user to the Patient Index' do
+
+    visit "/pediatricians/#{@pediatrician.id}/patients"
+
+    click_on "Patients"
+
+    expect(current_path).to eq('/patients')
+  end
 end
