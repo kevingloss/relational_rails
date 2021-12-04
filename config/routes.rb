c11dccda0ff7456d03999a4aba53709f1c19efb0
Kevin Gloss <kgloss89@gmail.com>
2021-12-04 12:47:10 -0700

    Feature edit book from book show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   post '/authors', to: 'authors#create'
   get '/authors/:id/edit', to: 'authors#edit'
   patch '/authors/:id', to: 'authors#update'
+
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
+  get '/books/:id/edit', to: 'books#edit'
+  patch '/books/:id', to: 'books#update'
+
   get '/authors/:author_id/books', to: 'author_books#index'
   get '/authors/:author_id/books/new', to: 'author_books#new'
   post '/authors/:author_id/books', to: 'author_books#create'
