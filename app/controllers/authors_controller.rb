04da90006d04b7a69fb83110bd2f269bcc3fc805
Kevin Gloss <kgloss89@gmail.com>
2021-12-04 08:37:07 -0700

    Feature new and create routes/controller actions

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,17 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    author = Author.create(author_params)
+    redirect_to "/authors"
+  end
+
+private
+  def author_params
+    params.permit(:name, :age, :alive)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/authors', to: 'authors#index'
+  get '/authors/new', to: 'authors#new'
   get '/authors/:id', to: 'authors#show'
+  post '/authors', to: 'authors#create'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
   get '/authors/:author_id/books', to: 'author_books#index'
-
 
 
 
