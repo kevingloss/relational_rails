3b29c2e0c1d2a4084b61691a291435de06b360d0
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-05 09:56:41 -0700

    Add functionality for Editing a Pediatrician

--- app/controllers/pediatricians_controller.rb
+++ app/controllers/pediatricians_controller.rb
@@ -6,9 +6,20 @@ class PediatriciansController < ApplicationController
   def new
   end
 
+  def edit
+    @doctor = Pediatrician.find(params[:id])
+  end
+
   def create
     doctor = Pediatrician.create(pediatrician_params)
     redirect_to '/pediatricians'
+  end
+
+  def update
+    #require "pry"; binding.pry
+    @doctor = Pediatrician.find(params[:id])
+    @doctor.update(edit_pediatrician_params)
+    redirect_to "/pediatricians/#{@doctor.id}"
   end
 
   def show
@@ -17,5 +28,9 @@ class PediatriciansController < ApplicationController
 
   def pediatrician_params
     params.permit(:name, :office, :years_practicing, :accepting_patients)
+  end
+
+  def edit_pediatrician_params
+    params.permit(:office, :years_practicing, :accepting_patients)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,13 @@ Rails.application.routes.draw do
   get '/pediatricians', to: 'pediatricians#index'
   get '/pediatricians/new', to: 'pediatricians#new'
   get '/pediatricians/:id', to: 'pediatricians#show'
+  get '/pediatricians/:id/edit', to: 'pediatricians#edit'
   get '/patients', to: 'patients#index'
   get '/patients/:id', to: 'patients#show'
   get '/pediatricians/:id/patients', to: 'pediatrician_patients#index'
 
   post '/pediatricians', to: 'pediatricians#create'
+  patch '/pediatricians/:id', to: 'pediatricians#update'
 
 
 
