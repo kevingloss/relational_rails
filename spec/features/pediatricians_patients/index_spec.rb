ec311ddcc55a59b1f00dcadc88accd2a35a63ef3
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-05 13:09:02 -0700

    Update syntax to clean up test

--- spec/features/pediatricians_patients/index_spec.rb
+++ spec/features/pediatricians_patients/index_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Pediatricians Patients Index' do
   before :each do
     @pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
-    @pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
     @baby = Patient.create!(name: 'Elora Mielke', age_in_months: 2, full_term_birth: true, pediatrician_id: @pediatrician.id) 
   end
 
