46400d1c930cebd09b0d395fba25aeda8f4d79c8
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-05 16:44:05 -0700

    Add test for Pediatricians edit button

--- spec/features/pediatricians/index_spec.rb
+++ spec/features/pediatricians/index_spec.rb
@@ -1,43 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'Pediatrician index page' do
-  before :each do
-    @pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
-    @pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
-    @pediatrician_3 = Pediatrician.create!(name: 'Barbaro Baubau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+  describe 'display' do
+    before :each do
+      @pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
+      @pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+      @pediatrician_3 = Pediatrician.create!(name: 'Barbaro Baubau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+    end
+    it 'displays a list of pediatricians' do
+
+      visit "/pediatricians"
+
+      expect(page).to have_content(@pediatrician.name)
+      expect(page).to have_content(@pediatrician_2.name)
+      expect(page).to have_content(@pediatrician_3.name)
+    end
+
+    it 'displays the Pediatricians in order of creation, first to last' do
+
+      visit "/pediatricians"
+
+      expect(@pediatrician_3.name).to appear_before(@pediatrician_2.name)
+      expect(@pediatrician_2.name).to appear_before(@pediatrician.name)
+    end
+
+    it 'displays a link to take the user to the Pediatricians Index' do
+
+      visit "/pediatricians"
+
+      click_on "Pediatricians"
+
+      expect(current_path).to eq('/pediatricians')
+    end
+
+    it 'displays a link to take the user to the Patient Index' do
+
+      visit "/pediatricians"
+
+      click_on "Patients"
+
+      expect(current_path).to eq('/patients')
+    end
   end
-  it 'displays a list of pediatricians' do
 
-    visit "/pediatricians"
+  describe 'it displays an edit button' do
+    it 'displays a link that lets the user edit a specific Pediatrician' do
+      pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
+      visit '/pediatricians'
 
-    expect(page).to have_content(@pediatrician.name)
-    expect(page).to have_content(@pediatrician_2.name)
-    expect(page).to have_content(@pediatrician_3.name)
-  end
+      click_on('Edit Information', :match => :prefer_exact)
 
-  it 'displays the Pediatricians in order of creation, first to last' do
-
-    visit "/pediatricians"
-
-    expect(@pediatrician_3.name).to appear_before(@pediatrician_2.name)
-    expect(@pediatrician_2.name).to appear_before(@pediatrician.name)
-  end
-
-  it 'displays a link to take the user to the Pediatricians Index' do
-
-    visit "/pediatricians"
-
-    click_on "Pediatricians"
-
-    expect(current_path).to eq('/pediatricians')
-  end
-
-  it 'displays a link to take the user to the Patient Index' do
-
-    visit "/pediatricians"
-
-    click_on "Patients"
-
-    expect(current_path).to eq('/patients')
-  end
+      expect(current_path).to eq("/pediatricians/#{pediatrician.id}/edit")
+    end
+  end 
 end
