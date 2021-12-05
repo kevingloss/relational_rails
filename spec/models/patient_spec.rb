6a55b010fc4e7781c572d494c354ccf83a1974e0
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-05 14:20:26 -0700

    Add test for Patient .full_term?

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Patient do
   it {should belong_to :pediatrician}
+
+  before :each do
+    @pediatrician = Pediatrician.create!(name: 'Bob Barker', office: '123 Fake Street', years_practicing: 15, accepting_patients: true)
+    @pediatrician_2 = Pediatrician.create!(name: 'Roberto Guauguau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+    @pediatrician_3 = Pediatrician.create!(name: 'Barbaro Baubau', office: '123 Seaseme Street', years_practicing: 10, accepting_patients: false)
+
+    @baby = Patient.create!(name: 'Elora Mielke', age_in_months: 2, full_term_birth: true, pediatrician_id: @pediatrician.id)
+    @baby_2 = Patient.create!(name: 'Ezra Bridger', age_in_months: 8, full_term_birth: true, pediatrician_id: @pediatrician.id)
+    @baby_3 = Patient.create!(name: 'Kanan Jarrus', age_in_months: 11, full_term_birth: false, pediatrician_id: @pediatrician_2.id)
+  end
+
+  it 'can return a list of Patients born at full term' do
+
+    expect(Patient.full_term?).to eq([@baby, @baby_2])
+  end
 end
